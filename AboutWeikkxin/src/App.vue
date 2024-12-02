a432bd19df7ae54432ff47fae521754da505e84e
COSONWEI <2168642113@qq.com>
2024-12-02 10:39:38 +0800

    fix:删除了一些可以的链接并且修改了一些配置

--- AboutWeikkxin/src/App.vue
+++ AboutWeikkxin/src/App.vue
@@ -5,13 +5,19 @@ import MyFooter from './components/MyFooter.vue'
 </script>
 
 <template>
-  <div class="min-h-screen ">
-    <MyHeader />
-    <Home />
-    <MyFooter />
+  <div class="common-layout">
+    <el-container style="display: flex; flex-direction: column;min-height: 90vh;">
+      <el-header>
+        <MyHeader />
+      </el-header>
+      <el-main>
+        <Home  />
+      </el-main>
+      <el-footer>
+        <MyFooter />
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
