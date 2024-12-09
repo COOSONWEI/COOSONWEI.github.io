c9b67959542de4256ce50c3e8cf3d9530b7cbb0e
COSONWEI <2168642113@qq.com>
2024-12-10 00:00:24 +0800

    fix: 添加了一些脚本文件

---
+++ AboutWeikkxin/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+// 将打包后的静态文件目录设置为可访问的
+app.use(express.static(path.join(__dirname, 'dist')));
+// 对于所有的请求，都返回index.html，这是单页应用（SPA）的常见做法
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
